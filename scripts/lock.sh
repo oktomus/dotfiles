56624f99e57977d6b7229ab4eca10d5b3b935a13
Kevin Masson <contact.kevinmasson@gmail.com>
2017-07-23 17:22:30 +0200

    Lock the screen only if the mouse is not in the corner

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f /home/oktomus/Images/icons/lock-icon.png ]] && convert /tmp/screen.png /home/oktomus/Images/icons/lock-icon.png -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
+# Get Mouse pos
+eval $(xdotool getmouselocation --shell)
+
+# Dont lock if in the corner
+if [ $X -gt 90 ] || [ $Y -gt 90 ]
+then
+    scrot /tmp/screen.png
+    convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+    [[ -f /home/oktomus/Images/icons/lock-icon.png ]] && convert /tmp/screen.png /home/oktomus/Images/icons/lock-icon.png -gravity center -composite -matte /tmp/screen.png
+    i3lock -u -i /tmp/screen.png
+fi
+
+
