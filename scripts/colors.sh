7752eac54d4dfa610461d1d980cb811e0cdd3e2b
Kevin Masson <contact.kevinmasson@gmail.com>
2018-03-09 12:02:19 +0100

    polybar: remove colors

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -3,3 +3,15 @@
 for i in {0..255} ; do
   printf "\x1b[38;5;${i}m${i}  "
 done
+
+printf "\n\n"
+
+for i in {0..255} ; do
+  printf "\e[1;5m%-6s\e[${i}m" "${i}"
+done
+
+printf "\n\n"
+
+for i in {0..255} ; do
+  printf "\033[${i};${i}m${i} \033[0m"
+done
